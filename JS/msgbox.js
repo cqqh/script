533b883d9c50608b8c45ad9ba99b768646d256d0
cqqh <cqqh1@qq.com>
2015-01-15 16:14:47 +0800

    js对话框

--- JS/msgbox.js
+++ JS/msgbox.js
@@ -1,18 +1,31 @@
+/**
+ * 对话框
+ * @param {type} opt
+ * @returns {msgBox}
+ * @author cqqh
+ */
+
 var msgBox = function(opt){
     this.title = opt.title || '';
     this.content = opt.content || 'load...';
     this.width = opt.width || 300;
     this.height = opt.height || 180;
-    
+    this.isMask = opt.isMask || false;
+
     this.dom = document.createElement('div');
-    this.dom.className      = 'mask';
-    this.dom.style.position = 'fixed';
+    this.dom.style.position = 'absolute';
     this.dom.style.top      = '0px';
     this.dom.style.left     = '0px';
-    this.dom.style.width    = '100%';
-    this.dom.style.height   = '100%';
-    this.dom.style.zIndex   = 1000;
-    this.dom.style.backgroundColor = 'rgba(0,0,0,0.5)';
+
+    //遮罩层
+    if(this.isMask){
+        this.mask = '<div style="position: fixed;top: 0;left: 0;width: 100%;height: 100%;z-index: 1000;background: #000;opacity: 0.5;filter: alpha(opacity=50);"></div>';
+        this.dom.innerHTML = this.mask;
+    }
+    //
+    if(typeof this.content == 'object'){
+        this.content = this.content.html();
+    }
 
     this.init();
 }
@@ -22,14 +35,19 @@ msgBox.prototype = {
         this.getWindowSize();
         var posWidth = this.windowWidth/2 - this.width/2 + 'px',
             posHeight= this.windowHeight/2 - this.height/2 + 'px',
+            dom = this.dom,
             html = '';
-        html+= '<div style="padding: 5px;background: #fafafa;position: absolute;left:'+posWidth+';top:'+posHeight+';width:'+this.width+'px;height:'+this.height+'px">';
-        html+= '<div><h3>'+this.title+'</h3></div>';
+        html+= '<div style="border:1px solid #eee;z-index:9999;padding:3px;border-radius:5px;background: #fafafa;position: fixed;left:'+posWidth+';top:'+posHeight+';width:'+this.width+'px;height:'+this.height+'px">';
+        html+= '<span class="close" style="position: absolute;padding: 2px; top: 0; right: 2px; cursor: pointer;">X</span>';
+        html+= '<div class="title" style="text-align: center;padding: 5px;height: 20px;"><h3>'+this.title+'</h3></div>';
         html+= '<div>'+this.content+'</div>';
         html+= '</div>';
-        this.dom.innerHTML = html;
+        dom.innerHTML += html;
 
-        document.getElementsByTagName('body')[0].appendChild(this.dom);
+        document.body.appendChild(this.dom);
+        dom.getElementsByClassName('close')[0].onclick = function(){
+            dom.remove();
+        };
     },
     getWindowSize:function(){
         // 获取窗口宽度
